171382befd00c246741be001b81bab44aa473415
up泰和 <1710318399@qq.com>
2026-09-25 13:35:32 +0800

    Change throw macro to use do-while for safety

--- exception_for_c/exceptions.h
+++ exception_for_c/exceptions.h
@@ -12,7 +12,7 @@ const char* __exception_name__;
 #define try if(!setjmp(__try_point__[__curr__++]))
 #define catch(exception) else if(__at_exception__&&(!strcmp(__exception_name__,(#exception))||!strcmp("Exception",(#exception))))
 #define exception_clear {__at_exception__ = false;__exception_cl__ = true;++__curr__;}
-#define throw(...) {__at_exception__ = true;__exception_name__=*(#__VA_ARGS__)?#__VA_ARGS__:"Exception";if(__exception_cl__)__curr__--;__exception_cl__=false;longjmp(__try_point__[--__curr__],1);}
+#define throw(...) do{__at_exception__ = true;__exception_name__=*(#__VA_ARGS__)?#__VA_ARGS__:"Exception";if(__exception_cl__)__curr__--;__exception_cl__=false;longjmp(__try_point__[--__curr__],1);}while(false)
 #define finally if(true)
 #define try_end if(__at_exception__){longjmp(__try_point__[--__curr__],1);}else{__exception_cl__=false,--__curr__;}
 #define exception_init if(setjmp(__try_point__[__curr__++])){fprintf(stderr, "unhandled exception: %s\n", __exception_name__);exit(-1);}
